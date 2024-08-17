e92a0821815e394790447cd5b24ee05d3769d0f3
Ankush <drankush1989@gmail.com>
2024-08-17 14:21:55 +0530

    Add files via upload

---
+++ config copy.py
@@ -0,0 +1,19 @@
+OPENAI_API_URL = "https://api.openai.com/v1"
+OPENAI_API_KEY = "Your API key" 
+MODEL = "Model Name" 
+
+# Instruction prompts
+INSTRUCTION_PROMPTS = {
+    0: "Key Points Extraction: 'Extract key points from the following text:'",
+    1: "Summarization: 'Summarize the following text:'",
+    2: "Translation: 'Translate the following text into Spanish:'",
+    3: "Explanation: 'Explain the following text in detail:'",
+    4: "Question Answering: 'Answer the following question based on the text:'",
+    5: "Question Generation: 'Generate Questions based on the text:'",
+    6: "Paraphrasing: 'Paraphrase the following text:'",
+    7: "Sentiment Analysis: 'Determine the sentiment of the following text:'",
+    8: "Topic Modeling: 'Identify the topics in the following text:'",
+    9: "Text Simplification: 'Simplify the following text for easier understanding:'",
+    10: "Text Expansion: 'Expand the following text on the topic being discussed:'"
+
+}
